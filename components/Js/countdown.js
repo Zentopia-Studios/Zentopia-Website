4ad3b5b1c23bab70865f3a868cf49f0d7c424908
Gucci Fox <80353042+GucciFoxDev@users.noreply.github.com>
2023-02-03 16:07:01 -0500

    Updated countdown component and added comments

--- components/Js/countdown.js
+++ components/Js/countdown.js
@@ -1,21 +1,23 @@
-
-
-
-      var countDownDate = new Date("Mar 1, 2023 21:28:25").getTime();
-
-      var x = setInterval(function () {
-        var now = new Date().getTime();
-        var distance = countDownDate - now;
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor(
-          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-        );
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        document.getElementById("countdown").innerHTML =
-          days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-        if (distance < 0) {
-          clearInterval(x);
-          document.getElementById("countdown").innerHTML = "We have released!";
-        }
-      }, 1000);
+var countDownDate = new Date("Mar 1, 2023 21:28:25").getTime();
+var x = setInterval(function () {
+  // Important dates
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      
+  // Time measures until release
+  var hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+   );
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      
+  // Replace countdown element with time remaining
+  document.getElementById("countdown").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  // If time is 0, set countdown element to released text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "We have released!";
+  }
+}, 1000);
